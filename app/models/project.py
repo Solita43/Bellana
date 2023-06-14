7ef5aa0fe0fedc68fcee4834a9a130ecb3f8ea46
Melinda Cortez <meli.matrix@outlook.com>
2023-06-14 08:54:10 -0700

    added details column to projects and created flask project form

--- app/models/project.py
+++ app/models/project.py
@@ -10,6 +10,7 @@ class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(35), nullable=False)
     owner_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    details = db.Column(db.String(500))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     owner = db.relationship("User", back_populates="projects")
@@ -19,6 +20,7 @@ class Project(db.Model):
             "id": self.id,
             "name": self.name,
             "ownerId": self.owner_id,
+            "details": self.details,
             "createdAt": self.created_at
         }
 
