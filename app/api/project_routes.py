b664aec03ebc575b6e737a73c48a1e9b21d8f1fa
Melinda Cortez <meli.matrix@outlook.com>
2023-06-14 10:10:16 -0700

    added routes for creating, updating, and deleting projects

--- app/api/project_routes.py
+++ app/api/project_routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_login import login_required, current_user
-from app.models import Project
+from app.models import Project, db
+from app.forms import ProjectForm
 
 project_routes = Blueprint('projects', __name__)
 
@@ -9,6 +10,83 @@ project_routes = Blueprint('projects', __name__)
 @login_required
 def projects():
     """
-    Query for all users and returns them in a list of user dictionaries
+    Query for all projects and returns a dictionary of each project.
     """
+
+    return {project.id: project.to_dict() for project in Project.query.all()}
+
+@project_routes.route('/my-projects')
+@login_required
+def user_projects():
+    """
+    Return a dictionary of all projects the current user owns.
+    """
+
     return {project.id: project.to_dict() for project in current_user.projects}
+
+
+@project_routes.route('/', methods=["POST"])
+@login_required
+def create_project():
+    """
+    Creates a new project and returns the the new project dictionary.
+    """
+    form = ProjectForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    form.ownerId.data = current_user.id
+
+    if form.validate():
+        project = Project(
+            name=form.data["name"],
+            owner_id=form.data["ownerId"],
+            details=form.data["details"]
+        )
+
+        db.session.add(project)
+        db.session.commit()
+        return project.to_dict()
+    else:
+        return form.errors, 400
+
+@project_routes.route('/<int:projectId>', methods=["PUT"])
+@login_required
+def update_project(projectId):
+    """
+    Query for project by id, updates information, and returns the updated project dictionary.
+    """
+    project = Project.query.get(projectId)
+
+
+
+    form = ProjectForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate():
+        project.name=form.data["name"]
+        project.owner_id=form.data["ownerId"]
+        project.details=form.data["details"]
+
+        db.session.commit()
+        return project.to_dict(), 201
+    else:
+        return form.errors, 400
+    
+
+@project_routes.route('/<int:projectId>', methods=["DELETE"])
+@login_required
+def delete_project(projectId):
+    """
+    Query for project by id, removes project from database and returns message of successfully deleted.
+    """
+
+    project = Project.query.get(projectId)
+    
+    if not project:
+        return {"error": "Project not found..."}, 404
+    
+    if current_user.id != project.owner_id:
+        return {'error': 'Unauthorized'}, 401
+    
+    db.session.delete(project)
+    db.session.commit()
+    return {"message": "Project has been successfully deleted."}
