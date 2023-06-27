98a14c57e45953d5101b1ff4d2fcfd4f9b7e2fe7
Melinda Cortez <meli.matrix@outlook.com>
2023-06-26 17:43:42 -0700

    created a form for tasks

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -3,3 +3,4 @@ from .signup_form import SignUpForm
 from .project_form import ProjectForm
 from .board_form import BoardForm
 from .resource_form import ResourceForm
+from task_form import TaskForm
---
+++ app/forms/task_form.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired, ValidationError
+from app.models import User
+
+def user_exists(form, field):
+    # Checking if user exists
+    userId = field.data
+    user = User.query.get(userId)
+    if user:
+        raise ValidationError('User does not exist')
+    
+def valid_status(form, field):
+    # Checking if user exists
+    status = field.data
+    options = ['Not started', 'in progress', 'complete']
+    if status not in options:
+        raise ValidationError('That is not a valid status. Please choose from Not started, in progress, or complete')
+
+
+class TaskForm(FlaskForm):
+    details = StringField("details", validators=[DataRequired(), user_exists])
+    status = IntegerField("status", validators=[DataRequired(), valid_status])
+    userId = StringField("userId", validators=[DataRequired()])
