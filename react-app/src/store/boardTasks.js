c9792ba01c7d2acd358539d88afc42f9af09a79f
Melinda Cortez <meli.matrix@outlook.com>
2023-07-10 19:29:44 -0700

    Created store action and thunk for deleting a task

--- react-app/src/store/boardTasks.js
+++ react-app/src/store/boardTasks.js
@@ -1,5 +1,6 @@
 const GET_TASKS = "boardTasks/GET_TASKS";
 const ADD_TASK = "boardTasks/ADD_TASK";
+const DELETE_TASK ="boardTasks/DELETE_TASK";
 
 const getTasks = (tasks) => ({
     type: GET_TASKS,
@@ -9,6 +10,11 @@ const getTasks = (tasks) => ({
 const addTask = (task) => ({
     type: ADD_TASK,
     payload: task
+})
+
+const deleteTask = (taskId) => ({
+    type: DELETE_TASK,
+    payload: taskId
 })
 
 export const boardTasksGet = (boardId) => async (dispatch) => {
@@ -84,6 +90,20 @@ export const taskPut = (taskId, task) => async (dispatch) => {
     }
 }
 
+export const taskDelete = (taskId) => async (dispatch) => {
+    const res = await fetch(`/api/tasks/${taskId}`, {
+        method: "DELETE"
+    })
+
+    const data = await res.json()
+
+    if (res.ok) {
+        dispatch(deleteTask(taskId))
+    } else {
+        return data
+    }
+}
+
 const initialState = {}
 
 export default function reducer(state = initialState, action) {
@@ -92,6 +112,10 @@ export default function reducer(state = initialState, action) {
             return action.payload
         case ADD_TASK:
             return {...state, ...action.payload}
+        case DELETE_TASK:
+            const newState = {...state}
+            delete newState[action.payload]
+            return newState
         default:
             return state
     }
