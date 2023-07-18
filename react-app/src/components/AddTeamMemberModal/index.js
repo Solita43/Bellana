7c65b6c86d3478fcf4ec0d0d737f741bd5245ba8
Melinda Cortez <meli.matrix@outlook.com>
2023-07-18 09:46:54 -0700

    styling of team member modal

--- react-app/src/components/AddTeamMemberModal/index.js
+++ react-app/src/components/AddTeamMemberModal/index.js
@@ -14,13 +14,15 @@ function AddTeamMemberModal({ projectId }) {
     const dispatch = useDispatch();
     const usersObj = useSelector(state => state.users);
     const [firstName, setFirstName] = useState("")
-    const [chosenId, setChosenId] = useState(null)
+    const [chosenMember, setChosenMember] = useState(null)
     const [role, setRole] = useState("")
     const [admin, setAdmin] = useState(false)
     const [showSearch, setShowSearch] = useState(false);
     const ulRef = useRef()
 
-
+    const innerButton = (user) => {
+        return `${user.firstName[0]}${user.lastName[0]}`
+    }
 
 
     useEffect(() => {
@@ -51,10 +53,10 @@ function AddTeamMemberModal({ projectId }) {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        if (!chosenId) return
+        if (!chosenMember) return
 
         const new_member = {
-            userId: chosenId,
+            userId: chosenMember.id,
             projectId,
             role,
             admin
@@ -82,47 +84,77 @@ function AddTeamMemberModal({ projectId }) {
     return (
         <>
             <h1>Add a New Team Member</h1>
-            <div>
-                <input
-                    type="text"
-                    className="member-filter"
-                    value={firstName}
-                    onChange={(e) => setFirstName(e.target.value)}
-                    placeholder="Search by first name"
-                    onClick={openMenu}
-
-                ></input>
-                <ul className={ulClassName} ref={ulRef}>
-                    {users.map(user => {
-                        return (
-                            <li key={user.id} onClick={() => {
-                                setChosenId(user.id)
-                                setShowSearch(false)
-                            }}>
-                                {user.firstName} {user.lastName}
-                            </li>
-                        )
-                    })}
-                </ul>
-
-
-            </div>
-            <form onSubmit={handleSubmit} id="member-form">
-                <label>
-                    Role
+            <div className="member-search-choose">
+                <div className="member-filter">
                     <input
                         type="text"
-                        value={role}
-                        onChange={(e) => setRole(e.target.value)}
-                        minLength={4}
-                        className="member-role"
-                    />
-                </label>
-                <div className="member-admin">
-                    <p>Admin</p>
-                    <p className="admin-permissions">(Admins can add members, create, edit, and delete tasks, and assign tasks)</p>
-                    <input type="checkbox" id="toggle" class="offscreen" onChange={() => setAdmin(!admin)} checked={admin} />
-                    <label className="switch" htmlFor={`toggle`}></label>
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                        placeholder="Search by first name"
+                        onClick={openMenu}
+
+                    ></input>
+                    <div className="search-dropdown-container">
+                        <ul className={ulClassName} ref={ulRef}>
+                            {users.map(user => {
+                                return (
+                                    <li key={user.id} onClick={() => {
+                                        setChosenMember(user)
+                                        setShowSearch(false)
+                                    }}>
+                                        {user.firstName} {user.lastName}
+                                    </li>
+                                )
+                            })}
+                        </ul>
+
+                    </div>
+
+
+                </div>
+                <div className="chosen-member-wrapper">
+                    <h4>New Member Preview</h4>
+
+                    {chosenMember ? (
+                        <div className="current-member-container">
+                            <div id="profile-button">
+                                <p id="initials">{innerButton(chosenMember)}</p>
+                            </div>
+                            <div className="member-details">
+                                <p className="member-name">{chosenMember.firstName} {chosenMember.lastName}</p>
+                                <div className="member-role">
+                                    {role ? <p>{role}</p> : null}
+                                    {admin ? <i className="fa-solid fa-user-shield"></i> : null}
+                                </div>
+                            </div>
+                        </div>
+                    ) : (
+                        <p className="none-chosen">Search for a new team member...</p>
+                    )}
+                </div>
+            </div>
+            <form onSubmit={handleSubmit} >
+                <div id="member-form">
+                    <label>
+                        Role
+                        <input
+                            type="text"
+                            value={role}
+                            onChange={(e) => setRole(e.target.value)}
+                            minLength={4}
+                            className="member-role"
+                        />
+                    </label>
+                    <div className="member-admin">
+                        <div className="label-toggle-admin">
+                            <p>Admin</p>
+                            <div className="toggle-switch">
+                                <input type="checkbox" id="toggle" class="offscreen" onChange={() => setAdmin(!admin)} checked={admin} />
+                                <label className="switch" htmlFor={`toggle`}></label>
+                            </div>
+                        </div>
+                        <p className="admin-permissions">(Admins can add members, create, edit, and delete tasks, and assign tasks)</p>
+                    </div>
                 </div>
                 <button className="login-form" type="submit">Add Team Member</button>
 
