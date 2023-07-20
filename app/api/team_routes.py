63f5c49fea6c3b1de76c45d5f094c067588b204b
Melinda Cortez <meli.matrix@outlook.com>
2023-07-20 04:10:59 -0700

    Api route to change team member role

--- app/api/team_routes.py
+++ app/api/team_routes.py
@@ -33,3 +33,36 @@ def create_member(projectId):
         return new_member.to_dict(), 201
     else:
         return validation_errors_to_error_messages(form.errors), 400
+    
+@team_routes.route('/role/<int:memberId>', methods=["PUT"])
+@login_required
+def update_role(memberId):
+    member = TeamMember.query.get(memberId)
+
+    if not member:
+        return {"error": "Team Member not found"}, 404
+    
+    if not is_owner_admin(current_user.id, member.project_id):
+        return {"error": "Unauthorized"}, 401
+    
+    if member.owner and current_user.id != member.user_id:
+        return {"error": "Unauthorized"}, 401
+    
+    data = request.get_json()
+
+    if len(data["role"]) == 0:
+        member.role = None
+        db.session.commit()
+        return member.to_dict(), 201
+    elif len(data["role"]) <= 20:
+        member.role = data["role"]
+        db.session.commit()
+        return member.to_dict(), 201
+    else: 
+        print("😾😾😾😾😾😾😾😾😾😾😾😾😾😾😾😾😾 ERROR BLOCK =====> ")
+
+        return {"error": "Role cannot be greater than 20 characters"}
+        
+
+
+    
