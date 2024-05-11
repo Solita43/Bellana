9c0f02780689792a3bdb932dee0b7aa95aceab3f
Melinda Cortez <meli.matrix@outlook.com>
2024-05-10 22:19:35 -0700

    Reduce Queries from 3 to 1 for login route

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -6,8 +6,7 @@ from app.models import User
 
 def user_exists(form, field):
     # Checking if user exists
-    email = field.data
-    user = User.query.filter(User.email == email).first()
+    user = form.user
     if not user:
         raise ValidationError('Email provided not found.')
 
@@ -15,8 +14,7 @@ def user_exists(form, field):
 def password_matches(form, field):
     # Checking if password matches
     password = field.data
-    email = form.data['email']
-    user = User.query.filter(User.email == email).first()
+    user = form.user
     if not user:
         raise ValidationError('No such user exists.')
     if not user.check_password(password):
